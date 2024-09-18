e5564a273626296970660a27d844888a6c9a5441
adlynfakhreyz <adlynuniacc@gmail.com>
2024-09-18 10:31:29 +0700

    Update routing

--- main/urls.py
+++ main/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
-from main.views import show_main
+from main.views import show_main, create_product_entry, show_xml, show_json, show_xml_by_id, show_json_by_id
 
 app_name = 'main'
 
 urlpatterns = [
     path('', show_main, name='show_main'),
+    path('create-product-entry', create_product_entry, name='create_product_entry'), # routing untuk form
+    path('xml/', show_xml, name='show_xml'), # routing untuk melihat keseluruhan object dalam format xml
+    path('json/', show_json, name='show_json'), # routing untuk melihat keseluruhan object dalam format json
+    path('xml/<str:id>/', show_xml_by_id, name='show_xml_by_id'), # routing untuk melihat object dengan id tertentu dalam format xml
+    path('json/<str:id>/', show_json_by_id, name='show_json_by_id'), # routing untuk melihat object dengan id tertentu dalam format json
 ]
