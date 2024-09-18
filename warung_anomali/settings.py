4a8fa50f5333ffe9bf8fe4855ce7387766c1f631
adlynfakhreyz <adlynuniacc@gmail.com>
2024-09-18 10:26:16 +0700

    Menambahkan Skeleton

--- warung_anomali/settings.py
+++ warung_anomali/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'warung_anomali.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
