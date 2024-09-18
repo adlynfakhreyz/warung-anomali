8573b4092d967533e8ad72b20db254ed6bec258c
adlynfakhreyz <adlynuniacc@gmail.com>
2024-09-18 10:30:59 +0700

    Menambahkan fungsi

--- main/views.py
+++ main/views.py
@@ -1,12 +1,45 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from main.forms import ProductForm
+from main.models import Product
+from django.http import HttpResponse
+from django.core import serializers
 
-# Create your views here.
+# Fungsi yang menampilkan main
 def show_main(request):
+    product_entries = Product.objects.all()
+    
     context = {
         'project_name' : 'Warung Anomali',
         'npm' : '2306241713',
         'name': 'Andi Muhammad Adlyn Fakhreyza Khairi Putra',
-        'class': 'PBP B'
+        'class': 'PBP B',
+        'product_entries': product_entries
     }
 
     return render(request, "main.html", context)
+
+def create_product_entry(request):
+    form = ProductForm(request.POST or None)
+
+    if form.is_valid() and request.method == "POST":
+        form.save()
+        return redirect('main:show_main')
+
+    context = {'form': form}
+    return render(request, "create_product_entry.html", context)
+
+def show_xml(request):
+    data = Product.objects.all()
+    return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
+
+def show_json(request):
+    data = Product.objects.all()
+    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
+
+def show_xml_by_id(request, id):
+    data = Product.objects.filter(pk=id)
+    return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
+
+def show_json_by_id(request, id):
+    data = Product.objects.filter(pk=id)
+    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
